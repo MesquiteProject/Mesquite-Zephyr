d5207d9cf0dd3f85dfc60e108c16f814a972d41e
David Maddison <david.maddison@science.oregonstate.edu>
2018-05-02 11:49:31 -0700

    Removed wikispaces references

--- Source/mesquite/zephyr/IQTreeScore/IQTreeScore.java
+++ Source/mesquite/zephyr/IQTreeScore/IQTreeScore.java
@@ -1,7 +1,7 @@
 /* Mesquite.zephyr source code.  Copyright 2007 and onwards D. Maddison and W. Maddison. 
 
 Mesquite.zephyr is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.
-Zephry's web site is http://mesquitezephyr.wikispaces.com
+Zephry's web site is http://zephyr.mesquiteproject.org
 
 This source code and its compiled class files are free and modifiable under the terms of 
 GNU Lesser General Public License.  (http://www.gnu.org/copyleft/lesser.html)
