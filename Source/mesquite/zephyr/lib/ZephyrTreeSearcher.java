f4e55279b745ec462fcd2a2a1089729edf67bafa
wmaddisn <wmaddisn@mail.ubc.ca>
2025-06-24 14:26:19 -0700

    Small undo

--- Source/mesquite/zephyr/lib/ZephyrTreeSearcher.java
+++ Source/mesquite/zephyr/lib/ZephyrTreeSearcher.java
@@ -77,8 +77,6 @@ public abstract class ZephyrTreeSearcher extends ExternalTreeSearcher implements
 	 public void setMatrixSource(MatrixSourceCoord msource) {
 		 super.setMatrixSource(msource);
 		 this.matrixSourceTask = getMatrixSource();
-		 if (matrixSourceTask!= null)
-			 matrixSourceTask.setHiringCondition(getCharacterClass());
 	 }
 	public  void setOutputTextListener(OutputTextListener textListener){
 		if (runner != null)
